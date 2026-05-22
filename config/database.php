6f85f1a7cd0944ed2efb07de20b3978f604d3da7
WinterBambie <fiebredeagosto14@gmail.com>
2026-05-22 15:34:22 -0500

    primer commit

--- config/database.php
+++ config/database.php
@@ -1,18 +1,22 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db   = "edoc_actualizado";
-    private $user = "root";
-    private $pass = "";
+    private $host, $db, $user, $pass, $port;
+
+    public function __construct() {
+        $this->host = getenv('MYSQLHOST') ?: 'localhost';
+        $this->db   = getenv('MYSQLDATABASE') ?: 'edoc_actualizado';
+        $this->user = getenv('MYSQLUSER') ?: 'root';
+        $this->pass = getenv('MYSQLPASSWORD') ?: '';
+        $this->port = getenv('MYSQLPORT') ?: '3306';
+    }
 
     public function connect() {
         try {
             $conn = new PDO(
-                "mysql:host={$this->host};dbname={$this->db};charset=utf8",
-                $this->user,
-                $this->pass
+                "mysql:host={$this->host};port={$this->port};dbname={$this->db};charset=utf8",
+                $this->user, $this->pass
             );
-            $conn->setAttribute(PDO::ATTR_ERRMODE,        PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $conn;
         } catch (PDOException $e) {
