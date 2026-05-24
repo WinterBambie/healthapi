4dbb9193b823be583ea0168fbf49a0ed69dee375
WinterBambie <fiebredeagosto14@gmail.com>
2026-05-24 16:32:45 -0500

    conexion raylway

--- router/api.php
+++ router/api.php
@@ -1,5 +1,11 @@
 <?php
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
