5afcc6673649f9ffbf0fb29e16899509cbfbeb12
WinterBambie <fiebredeagosto14@gmail.com>
2026-05-24 16:09:41 -0500

    verificacion de conexion

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-require_once __DIR__ . '/router/api.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL,"eror del");
+
+echo "Backend funcionando";
