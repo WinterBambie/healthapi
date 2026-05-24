29de5c9d1d6b66e8a230d6d3489b9e4d7e4e65a7
WinterBambie <fiebredeagosto14@gmail.com>
2026-05-24 16:19:25 -0500

    subir cambios de conexion

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-echo "Backend funcionando";
+require_once __DIR__ . '/router/api.php';
+echo "funcionando";
