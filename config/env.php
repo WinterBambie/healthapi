d5818214bd600915e0d192347a2d91c7ad68a55a
WinterBambie <fiebredeagosto14@gmail.com>
2026-05-27 13:52:50 -0500

    subir

--- config/env.php
+++ config/env.php
@@ -1,11 +1,11 @@
 <?php
 define('APP_URL',      getenv('APP_URL')      ?: 'http://localhost');
-define('FRONTEND_URL', getenv('FRONTEND_URL') ?: 'http://localhost:5173');
-define('JWT_SECRET',   getenv('JWT_SECRET')   ?: 'clave_secreta');
+define('FRONTEND_URL', getenv('FRONTEND_URL') ?: 'https://softyhealth.vercel.app/');
+define('JWT_SECRET',   getenv('JWT_SECRET')   ?: 'clave_secreta_muy_larga_1234567890');
 
 define('MAIL_HOST',      getenv('MAIL_HOST')      ?: 'smtp.gmail.com');
-define('MAIL_PORT',      getenv('MAIL_PORT')       ?: 587);
-define('MAIL_USER',      getenv('MAIL_USER')       ?: '');
-define('MAIL_PASS',      getenv('MAIL_PASS')       ?: '');
-define('MAIL_FROM',      getenv('MAIL_FROM')       ?: '');
-define('MAIL_FROM_NAME', getenv('MAIL_FROM_NAME')  ?: 'softyhealth');
+define('MAIL_PORT',      getenv('MAIL_PORT')      ?: 587);
+define('MAIL_USER',      getenv('MAIL_USER')      ?: '');
+define('MAIL_PASS',      getenv('MAIL_PASS')      ?: '');
+define('MAIL_FROM',      getenv('MAIL_FROM')      ?: '');
+define('MAIL_FROM_NAME', getenv('MAIL_FROM_NAME') ?: 'softyhealth');
